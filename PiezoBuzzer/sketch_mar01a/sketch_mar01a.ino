7c7c1d000740cb42048bd845491e5d2e5c23d8fc
Brenden Krueger <bkruegercomputerscience@gmail.com>
2022-03-01 09:34:02 -0800

    PinMode Commit

--- PiezoBuzzer/sketch_mar01a/sketch_mar01a.ino
+++ PiezoBuzzer/sketch_mar01a/sketch_mar01a.ino
@@ -1,9 +1,12 @@
-void setup() {
-  // put your setup code here, to run once:
+// Programmer: Brenden Krueger
+// Date: 2-24-2022
 
-}
+int pos = 0;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void setup()    //pinMode identifies the pins
+{
+pinMode(A0, INPUT);
+pinMode(8, OUTPUT);
+pinMode(A1, INPUT);
+pinMode(A2, INPUT);
 }
