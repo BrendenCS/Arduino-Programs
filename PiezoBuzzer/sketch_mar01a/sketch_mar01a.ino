15948a7db3a237e3314ba1e851ad9e47aa898e4f
Brenden Krueger <bkruegercomputerscience@gmail.com>
2022-03-01 09:35:10 -0800

    High Tone Commit

--- PiezoBuzzer/sketch_mar01a/sketch_mar01a.ino
+++ PiezoBuzzer/sketch_mar01a/sketch_mar01a.ino
@@ -1,5 +1,5 @@
 // Programmer: Brenden Krueger
-// Date: 2-24-2022
+// Date: 3-1-2022
 
 int pos = 0;
 
@@ -9,4 +9,12 @@ pinMode(A0, INPUT);
 pinMode(8, OUTPUT);
 pinMode(A1, INPUT);
 pinMode(A2, INPUT);
+}
+
+void loop()
+{
+// if button press on A0 is detected
+if (digitalRead(A0) == HIGH) {
+tone(8, 440, 100); // play tone 57 (A4 = 440 Hz)
+}
 }
